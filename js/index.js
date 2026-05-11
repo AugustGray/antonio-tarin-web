34384cca6c648d42c942a1cf43c4d323d51c0183
August Gray <63071966+AugustGray@users.noreply.github.com>
2026-05-11 11:23:19 -0600

    updated content on landing page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-// Index
-$('#index').click(function() {
-   $('#toggle').toggleClass('active');
-   $('#overlay').toggleClass('open');
-  });
-
-// Toggle
-  $('#toggle').click(function() {
-     $(this).toggleClass('active');
-     $('#overlay').toggleClass('open');
-    });
+// Navigation - hide navbar on scroll down, show on scroll up
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var navbar = document.querySelector('.navbar');
+  if (navbar) {
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = '0';
+    } else {
+      navbar.style.top = '-' + navbar.offsetHeight + 'px';
+    }
+    prevScrollpos = currentScrollPos;
+  }
+};
